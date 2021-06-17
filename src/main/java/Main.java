b2238c688803385eb7e1270b94c1d4f135d4d5b6
Gerardo Zachary <gerardo.zachary@mercadolibre.com>
2021-06-17 14:46:58 -0300

    Add mail dependencies & sending logic

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,55 @@
+import io.reactivex.rxjava3.core.Observable;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+
 public class Main {
     public static void main(String[] varargs) throws InterruptedException {
 
+    }
+
+    public static String sendMail(String to) {
+        String from = "gera.zacha@gmail.com";
+        final String username = "gera.zacha@gmail.com";
+        final String password = "Ma1l$2021";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+            Message message = new MimeMessage(session);
+
+            message.setFrom(new InternetAddress(from));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(to));
+            message.setSubject("Reporting");
+
+            message.setContent(
+                    "<h1>Welcome user!</h1>",
+                    "text/html");
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+        return "";
     }
 }
